d50a2f436de59712fd0147818ac4709248664d0a
Alex Tran <lxdemikz@gmail.com>
2020-02-26 19:12:48 -0800

    added information for about page

--- src/components/subcomponent/about.js
+++ src/components/subcomponent/about.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './about.css'
-import { render } from '@testing-library/react';
 
 export function AboutPage() {
     // console.log("About Page works")
@@ -8,8 +7,35 @@ export function AboutPage() {
         <div
             className="about"
         >
+            <div className="self_profile" >
+                <h1>Alex Tran</h1>
 
-            <h1>Under Construction</h1>
+                <div className="self_profile_desc" >
+                    <h4>Education</h4>
+                    <ul>
+                        <li>Here</li>
+                        <li>Here</li>
+                        <li>& Here</li>
+                    </ul>
+                </div>
+
+                <div className="self_profile_desc" >
+                    <h4>Tools</h4>
+                    <ul>
+                        <li>Ruby</li>
+                        <li>Python</li>
+                        <li>Javascript</li>
+                        <li>CSS</li>
+                        <li>HTML</li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div className="self_description" >
+                <h3>About me</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
         </div>
     )
 }
